a2c9f3a1f7304782ec4904c16a0ec60659bfe2f8
hoonl <akbarrezaie6@gmail.com>
2025-08-14 17:46:14 +0200

    User, Product & Order models in the backend created.

---
+++ backend/models/productModel.js
@@ -0,0 +1,82 @@
+import mongoose from "mongoose"
+
+// Reviews Schema
+const reviewSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// Product Schema
+const productSchema = new mongoose.Schema(
+  {
+    // Reference to the user who created the product
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewsSchema],
+    rating: {
+      type: Number,
+      required: true,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Product = mongoose.model("Product", productSchema)
+
+export default Product
