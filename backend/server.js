9bb0fbf3602f0000ab4944623fc8f30b0370f5c3
hoonl <akbarrezaie6@gmail.com>
2025-08-16 15:04:36 +0200

    Getting data from DB

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,18 @@
-import express from 'express'
-import products from './data/products.js'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 dotenv.config()
+import productRoutes from "./routes/productRoutes.js"
 
 connectDB()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
+app.get("/", (req, res) => {
+  res.send("API is running...")
 })
-
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-// Single Product
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id)
-  res.json(product)
-})  
+app.use("/api/products", productRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
