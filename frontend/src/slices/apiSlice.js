6c1abdf864df10dfc0de6a6c5c77c82a4b311b01
hoonl <akbarrezaie6@gmail.com>
2025-08-16 17:15:17 +0200

    Working on frontend api

---
+++ frontend/src/slices/apiSlice.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import { BASE_URL } from "../constants"
+
+const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL })
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery,
+  tagTypes: ["Product", "User", "Order"],
+  endpoints: (builder) => ({
+    // getProducts: builder.query({
+    //   query: () => '/api/products',
+    // }),
+    // getProductById: builder.query({
+    //   query: (id) => `/api/products/${id}`,
+    // }),
+  }),
+})
