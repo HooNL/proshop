fbfe47cc4f648e64e1a81101f7d82c22cc7f45f7
hoonl <akbarrezaie6@gmail.com>
2025-08-16 15:24:33 +0200

    Custom Middleware added

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -22,11 +22,12 @@ router.get(
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
 
-    if (!product) {
-      res.status(404)
-      throw new Error("Product not found")
+    if (product) {
+        res.json(product)
+    } else {
+        res.status(404)
+        throw new Error("Product not found")
     }
-    res.json(product)
   })
 )
 
--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,23 @@ import dotenv from "dotenv"
 import connectDB from "./config/db.js"
 dotenv.config()
 import productRoutes from "./routes/productRoutes.js"
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
 connectDB()
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
+
+
 app.get("/", (req, res) => {
   res.send("API is running...")
 })
 app.use("/api/products", productRoutes)
+
+// Custom middleware
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
