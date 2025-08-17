2c3e8a2bf782c7b67bba17a3005d2df363a25319
hoonl <akbarrezaie6@gmail.com>
2025-08-17 21:03:06 +0200

    UI component is setup

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -4,15 +4,15 @@ import { BASE_URL } from "../constants"
 const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL })
 
 export const apiSlice = createApi({
-  reducerPath: "api",
+  //   reducerPath: "api",
   baseQuery,
   tagTypes: ["Product", "User", "Order"],
-  endpoints: (builder) => ({
-    // getProducts: builder.query({
-    //   query: () => '/api/products',
-    // }),
-    // getProductById: builder.query({
-    //   query: (id) => `/api/products/${id}`,
-    // }),
-  }),
+  endpoints: (builder) => ({}),
 })
+
+// getProducts: builder.query({
+//   query: () => '/api/products',
+// }),
+// getProductById: builder.query({
+//   query: (id) => `/api/products/${id}`,
+// }),
